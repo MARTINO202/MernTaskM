f0bf87daa56635e31440f7f4d5a780d7f66cef25
Chukwurie  Martins Chidera <mchukwurie@gmail.com>
2024-08-03 10:17:13 -0700

    url update

--- frontend/src/redux/features/api/taskAPI.js
+++ frontend/src/redux/features/api/taskAPI.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const taskAPI = createApi({
   reducerPath: "taskAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000",
+    baseUrl: "https://merntaskm.onrender.com", //http://localhost:3000
   }),
   endpoints: (build) => ({
     getTasks: build.query({
